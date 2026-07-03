c62b29ef470b89474b0aac6f75007882c1c06b08
sourodip19 <sourodipdey19@gmail.com>
2026-07-03 09:53:27 +0530

    Initialize Pac-Man project with canvas, asset loading, and maze rendering

--- script.js
+++ script.js
@@ -19,20 +19,102 @@ let pacManUp;
 let pacManDown;
 let wall;
 
+// map of the game 
+
+const map = [
+  "XXXXXXXXXXXXXXXXXXX",
+  "X........X........X",
+  "X.XXX.XXX.X.XXX.XXX",
+  "XOXXX.XXX.X.XXXOXXX",
+  "X.................X",
+  "X.XXX.X.XXXXX.X.XXX",
+  "X.....X...X...X...X",
+  "XXXXX.XXX X XXX.XXX",
+  "    X.X       X.X  ",
+  "XXXXX.X XX-XX X.XXX",
+  "     .  BPOR  .    ",
+  "XXXXX.X XXXXX X.XXX",
+  "    X.X       X.X  ",
+  "XXXXX.X XXXXX X.XXX",
+  "X........X........X",
+  "X.XXX.XXX.X.XXX.XXX",
+  "XO..X.....P.....O.X",
+  "XXX.X.X.XXXXX.X.XXX",
+  "X.....X...X...X...X",
+  "X.XXXXXXXXX.XXXXXX.",
+  "XXXXXXXXXXXXXXXXXXX",
+];
+
+class Tile {
+  constructor(x, y, width, height, image) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.image = image;
+  }
+
+  draw() {
+    context.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+}
+
+let walls = [];
+
+function loadMap() {
+  for (let r = 0; r < rowCount; r++) {
+    for (let c = 0; c < columnCount; c++) {
+      let tile = map[r][c];
+
+      if (tile == "X") {
+        walls.push(
+          new Tile(c * tileSize, r * tileSize, tileSize, tileSize, wall),
+        );
+      }
+    }
+  }
+}
+
+function draw() {
+  context.clearRect(0, 0, boardWidth, boardHeight);
+
+  for (let wall of walls) {
+    wall.draw();
+  }
+}
+
+
 window.onload = function () {
   board = document.getElementById("board");
+
   board.width = boardWidth;
   board.height = boardHeight;
+
   context = board.getContext("2d");
+
+  loadImage();
+
+  wall.onload = function () {
+    loadMap();
+
+    requestAnimationFrame(update);
+  };
+
 };
+
+function update() {
+  draw();
+
+  requestAnimationFrame(update);
+}
 
 function loadImage()
 {
     wall = new Image();
     wall.src = '/sprites/wall.png';
 
-    blueGhosht = new Image();
-    blueGhosht.src = '/sprites/blueGhost.png';
+    blueGhost = new Image();
+    blueGhost.src = '/sprites/blueGhost.png';
 
     orangeGhost = new Image();
     orangeGhost.src = '/sprites/orangeGhost.png';
@@ -61,3 +143,5 @@ function loadImage()
     cherry = new Image();
     cherry.src = "/sprites/cherry.png";
 }
+
+
